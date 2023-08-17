f40148dff4211d2ed3a9c3b0dc3d352e8b6c0a57
vitoriasoliveira <77559436+vitoriasoliveira@users.noreply.github.com>
2023-08-17 11:57:49 -0300

    Create while.kt

---
+++ Aula2/while.kt
@@ -0,0 +1,16 @@
+
+fun main() 
+{
+	var num = 0
+    
+    while(num < 10)
+    {
+        println("volta de numero $num")
+        num++ //increases num by 1 
+    }
+     while(num > 0)
+    {
+        println("volta de numero $num")
+        num-- //decreases num by 1 
+    }
+}
